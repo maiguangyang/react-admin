9e22bd61354e145ea5b7f86244758e1b21ce8c04
Marlon.M <maiguangyang@163.com>
2022-09-16 21:38:08 +0800

    增加示例

--- src/pages/home/about/index.tsx
+++ src/pages/home/about/index.tsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+interface UserDataItem {
+  id: string;
+  phone: string;
+  username: string;
+}
+
+interface UserDataFields {
+  data: UserDataItem[];
+}
+
+const GET_DOGS = gql`
+  query GetUsers {
+    users {
+      data {
+        id
+        phone
+        username
+      }
+    }
+  }
+`;
 
 const HomeAbout = () => {
+  const { loading, error, data } = useQuery(GET_DOGS);
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+
+  const users: UserDataFields = data.users;
+
   return (
-    <div>about</div>
+    <>
+      {
+        users.data.map(item => <div key={item.id}>{JSON.stringify(item)}</div>)
+      }
+    </>
   );
 };
 
