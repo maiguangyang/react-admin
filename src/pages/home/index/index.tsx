0d62a36f20d4ce16fba552c71cd16f050a19fb02
Marlon.M <maiguangyang@163.com>
2023-07-01 11:34:27 +0800

    修改文字

--- src/pages/home/index/index.tsx
+++ src/pages/home/index/index.tsx
@@ -3,7 +3,8 @@ import React, { FC } from 'react';
 const HomeIndex: FC = () => {
   return (
     <div style={{ color: '#666' }}>
-      <p style={{ lineHeight: '25px' }}>xxx是一个结合AI的低代码平台：主打的就是一个快速开发、快速上线，不想写的功能，别人帮你写</p>
+      <p style={{ lineHeight: '25px' }}>目前市面上的低代码系统，要么就是在线配置好，人家给你一个链接来使用。要么就是找一个开源的自己部署一套，这种是不具备物料功能，很多扩展功能实现起来很麻烦。</p>
+      <p style={{ lineHeight: '25px' }}>xxx是一个结合AI的低代码平台：主打的就是一个快速开发、快速上线，不想写的功能，别人帮你写。</p>
       <h2>1、简介</h2>
       <ul>
         <li style={{ lineHeight: '25px' }}>除了提供手动操作的方式，还结合了GPT，以聊天的方式，自动帮你生成一套系统</li>
@@ -13,8 +14,7 @@ const HomeIndex: FC = () => {
       <h2>2、物料市场</h2>
       <ul>
         <li style={{ lineHeight: '25px' }}>物料的定义：对需求功能的扩展，提供一键安装的方式，接入到系统中</li>
-        <li style={{ lineHeight: '25px' }}>面对用户需求，生成的代码只包含了基础的增删改查功能</li>
-        <li style={{ lineHeight: '25px' }}>例如一个“用户管理”功能，生成的代码只有增删改查，想要实现一个用户登录功能，那么这个登录功能就需要扩展了</li>
+        <li style={{ lineHeight: '25px' }}>例如一个“用户管理”功能，生成的代码只有增删改查，想要实现一个用户登录功能，那么就需要扩展了</li>
         <li style={{ lineHeight: '25px' }}>物料可以由需求方、提供方在发布的时候进行定价</li>
       </ul>
     </div>
--- src/pages/layout/components/footer/index.tsx
+++ src/pages/layout/components/footer/index.tsx
@@ -6,7 +6,7 @@ const FooterLayer: FC = () => {
   return (
     <Footer style={{ textAlign: 'center', padding: '12px 0' }}>
       <span style={{ padding: '0 10px' }}>©2022 All Rights Reserved</span>
-      <Link to='/' className='link_group_link' title='联系客服' onClick={e => e.preventDefault()}>联系客服</Link>
+      <Link to='/' className='link_group_link' title='联系我们' onClick={e => e.preventDefault()}>联系我们</Link>
     </Footer>
   );
 };
